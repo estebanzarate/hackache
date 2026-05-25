4061135b54e4a35537a542e40849127132a67504
Esteban Zárate <estebanzarateok@google.com>
2026-05-25 00:10:33 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://hackache.vercel.app',
   integrations: [mdx(), sitemap()],
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+  }),
 });
--- src/content/posts/linux.md
+++ src/content/posts/linux.md
@@ -3,5 +3,4 @@ title: Linux
 tags: [linux]
 lang: es
 order: 4
-group: linux
 ---
